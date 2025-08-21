dc261212e3a067f6b4acada753f2a3a6c849a027
Michał Wiliński <118672945+TeferiHero@users.noreply.github.com>
2025-08-21 08:06:26 +0200

    Removed unnecessary android version check

--- android/src/main/java/blufi/espressif/BlufiPlugin.java
+++ android/src/main/java/blufi/espressif/BlufiPlugin.java
@@ -383,7 +383,7 @@ public class BlufiPlugin implements FlutterPlugin, ActivityAware, MethodCallHand
   }
 
   private class BlufiCallbackMain extends BlufiCallback {
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+//    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
     @Override
     public void onGattPrepared(BlufiClient client, BluetoothGatt gatt, BluetoothGattService service,
                                BluetoothGattCharacteristic writeChar, BluetoothGattCharacteristic notifyChar) {
